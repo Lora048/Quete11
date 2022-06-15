821e1b5594d6ad8231584196233289f67a3bc0d2
Lora Perrichon <loraperrichon@gmail.com>
2022-06-15 22:15:53 +0200

    quete 11

---
+++ app.js
@@ -0,0 +1,15 @@
+const { setupRoutes } = require('./routes');
+const express = require('express');
+const app = express();
+const cookieParser = require("cookie-parser");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+setupRoutes(app);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
